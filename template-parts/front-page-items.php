f26c12b1ad7975e3deef90fc3d257682809e5bc5
Brian <brian@triplepro.nl>
2025-06-05 16:46:40 +0200

    Added dynamic fields for homepage-items

--- template-parts/front-page-items.php
+++ template-parts/front-page-items.php
@@ -1,32 +1,50 @@
 
-<section>
-    <div class="container grid md:grid-cols-2 gap-20 items-center">
-        <div>
-            <h2>Wonen, Leven & Ontspannen</h2>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               <br /><br />
-            <div><a href="blogs" class="primary-button">Bekijk blogs</a></div>
+
+<?php
+
+    // Template part for displaying homepage content blocks
+
+    if (have_rows('homepage_content_blokken')) :
+        while (have_rows('homepage_content_blokken')) : the_row(); 
+    ?>
+    <section class="content-blokken">	
+        <div class="container content-blok">
+           
+        <?php           
+            $afbeelding = get_sub_field('content_blok_afbeelding');
+            $afbeelding_url = $afbeelding ? $afbeelding['url'] : '';
+            $afbeelding_alt = $afbeelding ? $afbeelding['alt'] : '';
+
+            
+            $tekst = get_sub_field('content_blok_tekst');
+
+        
+            $knop_data = get_sub_field('content_blok_link_knop');
+            $toon_knop = $knop_data['knop_weergeven'] ?? false;
+            $knop_tekst = $knop_data['knop_tekst'] ?? '';
+            $knop_url = $knop_data['selecteer_een_pagina_of_bericht'] ?? '';
+
+            ?>
+            <div class="grid md:grid-cols-2 gap-20 items-center">
+                <?php if ($afbeelding_url) : ?>
+                    <div class="content-afbeelding">
+                        <img src="<?php echo esc_url($afbeelding_url); ?>" alt="<?php echo esc_attr($afbeelding_alt); ?>" />
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($tekst) : ?>
+                    <div class="content-tekst">
+                        <div><?php echo $tekst; ?></div><br>
+                        <?php if ($toon_knop && $knop_url) : ?>
+                            <a href="<?php echo esc_url($knop_url); ?>" class="primary-button"><?php echo ($knop_tekst); ?></a>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/chewy-3cAMUE3YAO8-unsplash.jpg" class="main-image">
-        </div>
-    </div>   
-</section>
+    </section>
+    <?php
 
-
-<section>
-    <div class="container grid md:grid-cols-2 gap-20 items-center">         
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/sonnie-hiles-iANAdaNu7mg-unsplash.jpg" class="main-image">
-        </div>
-        <div>
-            <h2>Van huis tot tuin</h2>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.       
-            <br /><br />
-            <div><a href="blogs" class="primary-button">linkpartners</a></div>
-        </div>
-    </div>   
-</section>
-
-
-
+    endwhile;
+endif;
+?>
