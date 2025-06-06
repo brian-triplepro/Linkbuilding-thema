d5f7305765b153a9de3e9695a9396daf8e77a7bc
Brian <brian@triplepro.nl>
2025-06-06 10:33:34 +0200

    Added support for colors

--- header.php
+++ header.php
@@ -7,8 +7,24 @@
 </head>
 <body <?php body_class(); ?>>
 
+<?php
+  $text_color = get_field('text_color', 'option');
+  $bg_color = get_field('bg_color', 'option');
+  $btn_color = get_field('btn_color', 'option');
+  $container_width = get_field('container_width', 'option');
+?>
+
+<style>
+  :root {
+      --text-color: <?= esc_attr($text_color ?: '#807045'); ?>;
+      --bg-color: <?= esc_attr($bg_color ?: '#fbf7f1'); ?>;
+      --btn-color: <?= esc_attr($btn_color ?: '#cc7d47'); ?>;
+      --container-width: <?= esc_attr($container_width ?: '1300px'); ?>;
+  }
+</style>
+
 <header>
-  <div class="container flex justify-between items-center">
+  <div class="container flex justify-between items-center top-bar">
     <div class="logo">
     <a href="<?php echo home_url(); ?>">
       <?php 
@@ -43,7 +59,13 @@
         <?php wp_nav_menu(['theme_location' => 'top-menu', 'container_class' => 'mobile-hoofdmenu']); ?>
         <?php wp_nav_menu(['theme_location' => 'category-menu', 'container_class' => 'mobile-categorie-menu']); ?>
       </div>
-      <button class="close-mobile-menu">X</button>
+      <button class="close-mobile-menu">
+        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="24" height="24" fill="white"/>
+          <path d="M7 17L16.8995 7.10051" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M7 7.00001L16.8995 16.8995" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </button>
     </div>
 </header>
 
